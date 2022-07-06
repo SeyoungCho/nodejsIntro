82008f65a03aa1bb4677dd51411539a0c2bba1d2
SeyoungCho <wnl383@naver.com>
2022-07-06 14:51:30 +0900

    refactor

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var http = require('http');
 var fs = require('fs');
 var app = http.createServer(function(request, response) {
     var url = request.url;
-    if(request.url == '/') {
+    if(url == '/') {
         url = '/index.html';
     }
-    if(request.url == '/favicon.ico') {
+    if(url == '/favicon.ico') {
         return response.writeHead(404);
     }
     response.writeHead(200);

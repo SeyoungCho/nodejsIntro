a01401e1c1220f17327595090835800405c6059b
SeyoungCho <wnl383@naver.com>
2022-07-07 13:48:31 +0900

    feat: implement home page and 404Notfound page

--- main.js
+++ main.js
@@ -4,13 +4,63 @@ var url = require('url');
 var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    if(_url == '/') {
-        _url = '/index.html';
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){
+      if(queryData.id === undefined){ //home
+        var title = 'welcome'
+        var description = "Hello, Node.js";
+        var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=Javascript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p style="margin-top:45px;">
+              ${description}
+            </p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+      }
+      else {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, content){
+          var title = queryData.id;
+          var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <ul>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=Javascript">JavaScript</a></li>
+              </ul>
+              <h2>${title}</h2>
+              <p style="margin-top:45px;">
+                ${content}
+              </p>
+            </body>
+            </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      }
     }
-    if(_url == '/favicon.ico') {
-        return response.writeHead(404);
-    }
-    response.writeHead(200);
-    response.end(queryData.id);
-});
+  });
 app.listen(3000);
